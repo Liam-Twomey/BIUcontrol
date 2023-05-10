f32808ac56fed75688c1319a9e213501bf2ae110
Liam Twomey <61964255+Liam-Twomey@users.noreply.github.com>
2023-05-10 16:11:46 -0700

    Updated files for working modified BIU device

--- BIUpinlist.py
+++ BIUpinlist.py
@@ -1,6 +1,6 @@
 # this file contains a list of RPi GPIO pins used by BIU
 cannon         = 26 #
-plunger        = 19 #
+plunger        = 5#19 #
 dht22          = 24 # Temperature and humidity monitor - currently not used
 filterposition = 13 #
 sensorpower    = 6  #
