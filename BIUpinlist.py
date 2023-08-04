cc903b428dfd8f7599dae8e9b908f1c5c8111c61
Liam Twomey <61964255+Liam-Twomey@users.noreply.github.com>
2023-08-04 11:02:52 -0700

    Updated BIUpinlist.py for non-fried pins.

--- BIUpinlist.py
+++ BIUpinlist.py
@@ -1,9 +1,9 @@
 # this file contains a list of RPi GPIO pins used by BIU
 cannon         = 26 #
-plunger        = 19 # for 4B with correct ribbon cables
+plunger        = 13 # for 4B with correct ribbon cables
 #plunger        = 5 # for RPi3B+ due to burnt pins
 dht22          = 24 # Temperature and humidity monitor - currently not used
-filterposition = 13 #
+filterposition = 19 #
 sensorpower    = 6  #
 pedalsensor    = 20 #  
 interlock      = 21 #  
