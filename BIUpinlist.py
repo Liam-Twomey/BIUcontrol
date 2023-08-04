a4749e86f77bcbd747f99914dfda05bc64cb8ac9
biu1 <lptwomey@ucdavis.edu>
2023-08-04 13:26:10 -0700

    Fixed pin numbering in BIUpinlist.py

--- BIUpinlist.py
+++ BIUpinlist.py
@@ -1,11 +1,11 @@
 # this file contains a list of RPi GPIO pins used by BIU
 # For intact pins (RPi 4B)
 cannon         = 26 #
-#plunger        = 19 #
+plunger        = 19 #
 dht22          = 24 # Temperature and humidity monitor - currently not used
 filterposition = 13 #
 sensorpower    = 6  #
 pedalsensor    = 20 #  
 interlock      = 21 #
 # Modifications for my 3B+ which has some fried pins. (comment equivalent above to use)
-plunger = 5 #
+#plunger = 5 #
